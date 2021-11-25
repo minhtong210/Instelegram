daa566e3d067b841ca0e69a3d336b97f2a78f0d4
minhtong210 <minhtong210@gmail.com>
2021-11-25 13:15:34 +0700

    Update gitignore

--- Instelegram/Domain/Model/BaseModel.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import ObjectMapper
-
-protocol BaseModel: Mappable {
-    
-}
--- Pods/Target Support Files/RxUIAlert/RxUIAlert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxUIAlert : NSObject
-@end
-@implementation PodsDummy_RxUIAlert
-@end
